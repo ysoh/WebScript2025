8a61cdbd3c5c5d08ccc11b563a8397ae4da42e02
Yunsang Oh <ysoh0908@gmail.com>
2025-09-03 13:32:23 +0900

    2025-09-03 13:32

--- js/script_02.js
+++ js/script_02.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     const targetImg = document.getElementById('targetImg');
     document.addEventListener('mousemove', (e) => {
-        targetImg.style.left = `${e.clientX}px`;
-        targetImg.style.top = `${e.clientY}px`;
+        targetImg.style.left = e.clientX + 'px';
+        targetImg.style.top = e.clientY + 'px';
     });
 };
